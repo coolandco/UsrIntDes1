48934a9dffd9efb8bc056a23c00ec9e04b88efc2
Leo (Desktop PC) <leonhard.lerbs@hs-furtwangen.de>
2017-04-23 22:15:43 +0200

    start gui

--- UsrIntDes1/src/userInterfaceSwing/LoginAAS.java
+++ UsrIntDes1/src/userInterfaceSwing/LoginAAS.java
@@ -15,12 +15,17 @@ import java.awt.Insets;
 import java.awt.GridLayout;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import java.awt.Panel;
+import java.awt.Component;
+import java.awt.Rectangle;
+import java.awt.Dimension;
+import javax.swing.JRadioButton;
 
 public class LoginAAS extends JFrame {
 
 	private JPanel contentPane;
-	private final JPanel panel = new JPanel();
-	private JTextField textField;
+	private JTextField txtName;
+	private JTextField txtPasswort;
 
 	/**
 	 * Launch the application.
@@ -42,76 +47,54 @@ public class LoginAAS extends JFrame {
 	 * Create the frame.
 	 */
 	public LoginAAS() {
-		setExtendedState(Frame.MAXIMIZED_BOTH);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{1, 0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		contentPane.setLayout(gbl_contentPane);
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.insets = new Insets(0, 0, 5, 5);
-		gbc_panel.gridx = 0;
-		gbc_panel.gridy = 0;
-		contentPane.add(panel, gbc_panel);
-		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+		contentPane.setLayout(new GridLayout(5, 1, 5, 5));
 		
-		JButton btnNewButton = new JButton("New button");
-		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton.gridx = 1;
-		gbc_btnNewButton.gridy = 1;
-		contentPane.add(btnNewButton, gbc_btnNewButton);
+		Panel panel_0 = new Panel();
+		contentPane.add(panel_0);
+		panel_0.setLayout(new BoxLayout(panel_0, BoxLayout.X_AXIS));
 		
-		JButton btnNewButton_1 = new JButton("New button");
-		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
-		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 0);
-		gbc_btnNewButton_1.gridx = 2;
-		gbc_btnNewButton_1.gridy = 1;
-		contentPane.add(btnNewButton_1, gbc_btnNewButton_1);
+		JButton btnBeenden = new JButton("Beenden");
+		panel_0.add(btnBeenden);
 		
-		JLabel lblLogin = new JLabel("Login:");
-		GridBagConstraints gbc_lblLogin = new GridBagConstraints();
-		gbc_lblLogin.insets = new Insets(0, 0, 5, 5);
-		gbc_lblLogin.gridx = 1;
-		gbc_lblLogin.gridy = 3;
-		contentPane.add(lblLogin, gbc_lblLogin);
+		JButton btnLogin = new JButton("Login");
+		panel_0.add(btnLogin);
 		
-		JLabel lblBenutzername = new JLabel("Benutzername:");
-		GridBagConstraints gbc_lblBenutzername = new GridBagConstraints();
-		gbc_lblBenutzername.anchor = GridBagConstraints.EAST;
-		gbc_lblBenutzername.insets = new Insets(0, 0, 5, 5);
-		gbc_lblBenutzername.gridx = 1;
-		gbc_lblBenutzername.gridy = 4;
-		contentPane.add(lblBenutzername, gbc_lblBenutzername);
+		Panel panel_1 = new Panel();
+		contentPane.add(panel_1);
+		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
 		
-		textField = new JTextField();
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(0, 0, 5, 0);
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 2;
-		gbc_textField.gridy = 4;
-		contentPane.add(textField, gbc_textField);
-		textField.setColumns(10);
+		txtName = new JTextField();
+		txtName.setText("name");
+		panel_1.add(txtName);
+		txtName.setColumns(10);
 		
-		JLabel lblPasswort = new JLabel("Passwort:");
-		GridBagConstraints gbc_lblPasswort = new GridBagConstraints();
-		gbc_lblPasswort.insets = new Insets(0, 0, 5, 5);
-		gbc_lblPasswort.gridx = 1;
-		gbc_lblPasswort.gridy = 5;
-		contentPane.add(lblPasswort, gbc_lblPasswort);
+		Panel panel_2 = new Panel();
+		contentPane.add(panel_2);
+		panel_2.setLayout(new BoxLayout(panel_2, BoxLayout.X_AXIS));
 		
-		JLabel lblRolle = new JLabel("Rolle:");
-		GridBagConstraints gbc_lblRolle = new GridBagConstraints();
-		gbc_lblRolle.insets = new Insets(0, 0, 5, 5);
-		gbc_lblRolle.gridx = 1;
-		gbc_lblRolle.gridy = 6;
-		contentPane.add(lblRolle, gbc_lblRolle);
+		txtPasswort = new JTextField();
+		txtPasswort.setText("passwort");
+		panel_2.add(txtPasswort);
+		txtPasswort.setColumns(10);
+		
+		Panel panel_3 = new Panel();
+		contentPane.add(panel_3);
+		panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.X_AXIS));
+		
+		JRadioButton rdbtnSachbearbeiter = new JRadioButton("Sachbearbeiter");
+		panel_3.add(rdbtnSachbearbeiter);
+		
+		JRadioButton rdbtnAdmin = new JRadioButton("Admin");
+		panel_3.add(rdbtnAdmin);
+		
+		Panel panel_4 = new Panel();
+		contentPane.add(panel_4);
+		panel_4.setLayout(new BoxLayout(panel_4, BoxLayout.X_AXIS));
 	}
 
 }

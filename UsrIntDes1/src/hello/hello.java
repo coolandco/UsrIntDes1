ce7d8321b91573e371620582867c5287e2c5c328
Leo (Desktop PC) <leonhard.lerbs@hs-furtwangen.de>
2017-05-09 13:06:24 +0200

    anpassung zu jsf

---
+++ UsrIntDes1/src/hello/hello.java
@@ -0,0 +1,14 @@
+package hello;
+
+import javax.faces.bean.ManagedBean;
+@ManagedBean
+
+public class hello {
+	final String world = "hi welt!";
+	
+	public String getworld() {
+		return world;
+	}
+	
+
+}

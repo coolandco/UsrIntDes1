dfb40487e87b15c997591f4e78d705ba88de3150
Leo (Desktop PC) <leonhard.lerbs@hs-furtwangen.de>
2017-05-19 18:27:04 +0200

    working login

--- UsrIntDes1/src/schnittstellenschichtWeb/loginAAS.java
+++ UsrIntDes1/src/schnittstellenschichtWeb/loginAAS.java
@@ -1,23 +1,61 @@
 package schnittstellenschichtWeb;
 
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
-
+import entityClass.Sachbearbeiter;
+import kontrollschicht.LoginK;
 @ManagedBean
 
 public class loginAAS {
 
+	private LoginK kontrolle;
 
-	public String name = "name";
-	public String passwort = "passwort";
+	private String name = "name";
+	private String passwort = "passwort";
+	private String choosenRole = "1"; //Sachbearbeiter
 	
-	public void  login() {
-		LoggerWindow.log(name);
-		LoggerWindow.log(passwort);
+	
+	
+	public String  login() {
+		kontrolle = new LoginK();
 		
+		
+		kontrolle.enterName(name);
+		kontrolle.enterPassword(passwort);
+		
+		if (choosenRole.equals("2"))//admin
+			kontrolle.cooseAdmin("y");
+		else
+			kontrolle.cooseAdmin("n");
+		
+		
+		
+		
+		try {
+			kontrolle.führeLoginDurch();
+			
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage("frmCreds", new FacesMessage(e.getMessage(), e.getMessage()));
+			return "/loginAAS.xhtml";
+		}
+		
+		
+		//goto next page
+		if (choosenRole.equals("1"))//admin
+			return "/sbaAS.xhtml";
+		else
+			return "/admAS.xhtml";
 		
 	}
+	
+	public void bevoelkern(){
+		Sachbearbeiter.setHardImplementedLoginDetails();
+		
+	}
+
 
 	
 	public String getName() {
@@ -34,5 +72,15 @@ public class loginAAS {
 
 	public void setPasswort(String passwort) {
 		this.passwort = passwort;
+	}
+
+
+	public String getChoosenRole() {
+		return choosenRole;
+	}
+
+
+	public void setChoosenRole(String choosenRole) {
+		this.choosenRole = choosenRole;
 	}
 }

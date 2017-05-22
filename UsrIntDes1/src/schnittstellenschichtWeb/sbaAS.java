6979f791e60aa44e478500debd559b5ea01e8d1f
Leo <leonhard.lerbs@hs-furtwangen.de>
2017-05-22 11:56:57 +0200

    workin on it

--- UsrIntDes1/src/schnittstellenschichtWeb/sbaAS.java
+++ UsrIntDes1/src/schnittstellenschichtWeb/sbaAS.java
@@ -1,5 +1,8 @@
 package schnittstellenschichtWeb;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
 import kontrollschicht.SachbearbeiterS;
 import kontrollschicht.chooseUsrK;
 
@@ -9,7 +12,28 @@ public class sbaAS {
 	
 	
 	public String bearbeiten(){
-		return null;
+		System.out.println("1");
+		if(user.equals("")){
+			FacesContext.getCurrentInstance().addMessage("frmCreds", new FacesMessage(
+					"Es wurde kein benutzer ausgewählt"));
+			System.out.println("2");
+			return null;
+		} 
+		
+		chooseUsrK kontrolle = new chooseUsrK();
+		System.out.println("3");
+		try {
+			kontrolle.chooseUser(user);
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage("frmCreds", new FacesMessage(
+					e.getMessage()));
+			return null;
+		}
+		System.out.println("4");
+		
+		return "/changeUsrAS.xhtml?user=" + user;
+		
+		
 		
 	}
 
@@ -22,15 +46,17 @@ public class sbaAS {
 	
 
 	public String getUser() {//for jsf
-		
+		System.out.println("5");
 		return new chooseUsrK().getPossibleUserNames()[0]; // null pointer can happen
 	}
 
 	public void setUser(String user) {
+		System.out.println("7");
 		this.user = user;
 	}
 
 	public String[] getUsers() {
+		System.out.println("6");
 		return new chooseUsrK().getPossibleUserNames();
 	}
 }
